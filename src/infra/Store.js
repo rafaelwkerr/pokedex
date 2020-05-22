3eedc6fb5356234a66c6f9de3d264612ec0e2a0e
rafael.kerr <rafael.kerr@gmail.com>
2020-05-22 14:03:01 -0300

    add redux dev tools

--- src/infra/Store.js
+++ src/infra/Store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import appReducer from './Reducer'
 
-const store = createStore(appReducer);
+const store = createStore(appReducer, composeWithDevTools());
 
 export default store;
